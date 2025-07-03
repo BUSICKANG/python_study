3d9323b66a8bbc3bf4e081b187cab608b3ef3d9d
kangbusic <busic@outlook.kr>
2025-07-03 15:54:36 +0900

    7월3일 오후 실습자료

---
+++ python/변수와 수식.py
@@ -0,0 +1,8 @@
+x=int(input("x:"))
+y=int(input("y:"))
+print("두 수의 합", x+y)
+print("두 수의 차", x-y)
+print("두 수의 곱", x*y)
+print("두 수의 평균", (x+y)/2)
+print("두 수의 중 큰 수",max(x,y))
+print("두 수의 중 작은수", min(x,y))
