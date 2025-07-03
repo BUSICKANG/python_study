d02231bb4d2f67879ccddd05edb27d9037f915b1
kangbusic <busic@outlook.kr>
2025-07-03 16:22:25 +0900

    20250703 total_leg

---
+++ python/Skbs1.py
@@ -0,0 +1,15 @@
+# 각 동물의 수
+chickens = 2
+pigs = 4
+cows = 3
+
+# 각 동물의 다리 수
+chicken_legs = 2
+pig_legs = 4
+cow_legs = 4
+
+# 전체 다리 수 계산
+total_legs = (chickens * chicken_legs) + (pigs * pig_legs) + (cows * cow_legs)
+
+# 결과 출력
+print(f"전체 다리의 수는 {total_legs}개입니다.")
