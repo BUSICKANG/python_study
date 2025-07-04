83d328c2dff84243edd727fb863deed0830bcfda
kangbusic <busic@outlook.kr>
2025-07-04 10:48:15 +0900

    7월 4일 실습

--- python/Skbs7.py
+++ python/Skbs7.py
@@ -1,3 +1,3 @@
 num = int(input("4자리 정수 입력:"))
-digit_sum = (num // 1000) + (num % 1000 // 100) + (num % 100 // 10)
+digit_sum = (num // 1000) + (num % 1000 // 100) + (num % 100 // 10) + (num % 10 // 1)
 print("각 자릿수의 합:", digit_sum)
